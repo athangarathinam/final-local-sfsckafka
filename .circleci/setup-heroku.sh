9755b91164f2f87b5f79125bbf23bb0d8a22548a
Dallin Rasmuson <drasmuson@progrexion.com>
2020-12-02 14:18:08 -0700

    fix typo in config.yml file.

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -2,14 +2,14 @@
 curl -OSs https://cli-assets.heroku.com/install.sh
 sudo sh ./install.sh
 
-# cat > ~/.netrc << EOF
-# machine api.heroku.com
-#   login $HEROKU_LOGIN
-#   password $HEROKU_TOKEN
-# EOF
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_TOKEN
+EOF
 
-# cat >> ~/.ssh/config << EOF
-# VerifyHostKeyDNS yes
-# StrictHostKeyChecking no
-# EOF
-# chmod 600 ~/.netrc
+cat >> ~/.ssh/config << EOF
+VerifyHostKeyDNS yes
+StrictHostKeyChecking no
+EOF
+chmod 600 ~/.netrc
