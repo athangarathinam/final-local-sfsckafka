1bdd60f0ecb933ad0197c9bafff6458609a0b7be
athangarathinam <74936344+athangarathinam@users.noreply.github.com>
2020-12-09 03:02:36 +0530

    Added kafka Producer - JSON

--- app/kafka-producer.py
+++ app/kafka-producer.py
@@ -7,11 +7,15 @@ Created on Sat Nov 28 02:15:31 2020
 
 import os
 import psycopg2
+import kafka_helper
 
 from datetime import datetime
 
 KAFKA_TOPIC = 'salfrs_kafka_snowflake'
 PRODUCER = kafka_helper.get_kafka_producer()
+
+print("Kafka Topic - ", KAFKA_TOPIC, "\n")
+print("Kafka PRODUCER - ", PRODUCER, "\n")
 
 if __name__ == '__main__':
     try:
@@ -24,6 +28,9 @@ if __name__ == '__main__':
          cursor.execute(postgreSQL_select_Query)
 
          period_records = cursor.fetchall()
+            
+            period_JSON = '{{{}}}'.format(','.join(['{}:{}'.format(json.dumps(k), json.dumps(v)) for k, v in period_records]))
+            print("Period JSON", period_JSON)
 
          for row in period_records:
             print("Id =", row[3], "\n")
