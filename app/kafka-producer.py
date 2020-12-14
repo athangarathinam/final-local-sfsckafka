a86f2275feaf40bb936a9bb9d2b544ba23a8b42a
athangarathinam <74936344+athangarathinam@users.noreply.github.com>
2020-12-15 01:23:11 +0530

    Update kafka-producer.py

--- app/kafka-producer.py
+++ app/kafka-producer.py
@@ -1,7 +1,7 @@
 import snowflake.connector
 import os
 import json
-# import ssl
+import ssl
 import kafka_helper
 from kafka import KafkaProducer  # ,  KafkaConsumer
 
@@ -12,13 +12,37 @@ print("Kafka T_CERT", V_KAFKA_TRUSTED_CERT)
 V_SSL_CONTEXT = kafka_helper.get_kafka_ssl_context()
 print("SSL Context",V_SSL_CONTEXT)
 
-conn= snowflake.connector.connect(
-    account = 'wda05749',
-    user = 'ATHANGARATHINAM',
-    password = 'Pradev2023',
-    database = 'SALES_FORCE_POC',
-    schema = 'PUBLIC',
-    warehouse = 'WH_SF_KAFKA_POC')
-cur = conn.cursor()
-tablval = cur.execute("select * from test1").fetchall()
-print(tablval)
+KAFKA_TOPIC = 'salfrs_kafka_snowflake'
+
+
+# Create Producer Properties
+def fn_kafka_producer(acks='all',
+                      value_serializer=lambda v: json.dumps(v).encode('utf-8')):
+    kafkaprod = KafkaProducer(
+        bootstrap_servers=V_KAFKA_URL,
+        # key_serializer=key_serializer,
+        value_serializer=value_serializer,
+        ssl_context=V_SSL_CONTEXT,
+        acks=acks
+    )
+    return kafkaprod
+
+if __name__ == '__main__':
+    # Create the Producer
+    PRODUCER = fn_kafka_producer()
+
+    # Create a producer Record
+    PRODUCER.send(KAFKA_TOPIC, 'Hello Heroku!!')
+    PRODUCER.flush()
+    
+    # Connect Snowflake
+    conn= snowflake.connector.connect(
+        account = 'wda05749',
+        user = 'ATHANGARATHINAM',
+        password = 'Pradev2023',
+        database = 'SALES_FORCE_POC',
+        schema = 'PUBLIC',
+        warehouse = 'WH_SF_KAFKA_POC')
+    cur = conn.cursor()
+    tablval = cur.execute("select * from test1").fetchall()
+    print(tablval)
