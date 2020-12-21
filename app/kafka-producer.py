bf8bfe56553fb4c8189dfda04f1114cb2d90b662
athangarathinam <74936344+athangarathinam@users.noreply.github.com>
2020-12-22 04:32:58 +0530

    Update kafka-producer.py

--- app/kafka-producer.py
+++ app/kafka-producer.py
@@ -111,9 +111,9 @@ if __name__ == '__main__':
     
   for message in CONSUMER:
     print("Consumer Test Inside FOr loop @@@@@@@@@@@@@@ -- 123456789")
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+#     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+#                                           message.offset, message.key,
+#                                           message.value))
     print("Consumer Test After For loop @@@@@@@@@@@@@@ -- 123456789")
     print(message.value['Body'])  
     print("Consumer Test After Body Print @@@@@@@@@@@@@@ -- 123456789")
