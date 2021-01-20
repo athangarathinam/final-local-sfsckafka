71b534685440d3644cdc36490070bae9ef6524a0
athangarathinam <74936344+athangarathinam@users.noreply.github.com>
2021-01-21 00:13:29 +0530

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+cd /usr/share/java/plugins
+java -jar snowflake-kafka-connector-1.5.1.jar
+
+cd /usr/share/java/kafka-connect-jdbc
+java -jar snowflake-jdbc-connector-3.12.17.jar
+
+cd /etc/kafka-connect/jars
+java -jar bc-fips-1.0.2.jar
+
+cd /etc/kafka-connect/jars
+java -jar bcpkix-fips-1.0.5.jar
