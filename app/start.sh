61dbf9714995906a90c48f7aa104380f8b8a40ec
pgx-admin <48065542+pgx-admin@users.noreply.github.com>
2021-01-28 14:51:19 -0700

    Create start.sh
    script fixes

--- app/start.sh
+++ app/start.sh
@@ -1,34 +1,20 @@
-#!/bin/bash
-
-#cd /usr/share/java/plugins
-#java -jar snowflake-kafka-connector-1.5.1.jar
-
-#cd /usr/share/java/kafka-connect-jdbc
-#java -jar snowflake-jdbc-connector-3.12.17.jar
-
-#cd /etc/kafka-connect/jars
-#java -jar bc-fips-1.0.2.jar
-
-#cd /etc/kafka-connect/jars
-#java -jar bcpkix-fips-1.0.5.jar
-
 #!/usr/bin/env bash
 
-echo $HEROKU_APP_NAME
+echo $APP_NAME
 
-echo "======== HEROKU_APP_NAME ====="
+echo "======== $APP_NAME ====="
 
-SERVER_HOST=$HEROKU_APP_NAME.herokuapp.com
+SERVER_HOST=$APP_NAME.herokuapp.com
 SERVER_URL=https://$SERVER_HOST
 
 
-echo "======== Before PORT ====="
+echo "======== Before PORT =====" 
 
-#export CONNECT_REST_PORT=$PORT
-#export CONNECT_REST_ADVERTISED_HOST_NAME="$SERVER_HOST" 
+export CONNECT_REST_PORT=$PORT
+export CONNECT_REST_ADVERTISED_HOST_NAME="$SERVER_HOST" 
 
-export REST_PORT=$PORT
-export REST_ADVERTISED_HOST_NAME="$SERVER_HOST" 
+#export REST_PORT=$PORT
+#export REST_ADVERTISED_HOST_NAME="$SERVER_HOST" 
 
 echo "======== After PORT ====="
 
@@ -38,22 +24,24 @@ kafka_prefix=$(echo ${!prefix_env_var})
 kafka_url_env_var="$(echo $kafka_addon_name)_URL"
 postgres_addon_name=${POSTGRES_ADDON:-DATABASE}
 
+echo "Variables: $kafka_addon_name $prefix_env_var $kafka_prefix $kafka_url_env_var $postgres_addon_name "
+
 echo "======== After postgres_addon_name ====="
 
-#CONNECT_BOOTSTRAP_SERVERS=${!kafka_url_env_var//kafka+ssl:\/\//}
-BOOTSTRAP_SERVERS=${!kafka_url_env_var//kafka+ssl:\/\//}
+export CONNECT_BOOTSTRAP_SERVERS=${!kafka_url_env_var//kafka+ssl:\/\//}
+#BOOTSTRAP_SERVERS=${!kafka_url_env_var//kafka+ssl:\/\//}
 
-#CONNECT_GROUP_ID=$(echo $kafka_prefix)connect-cluster
-GROUP_ID=$(echo $kafka_prefix)connect-cluster
+export CONNECT_GROUP_ID=$(echo $kafka_prefix)connect-cluster
+#GROUP_ID=$(echo $kafka_prefix)connect-cluster
 
-#CONNECT_OFFSET_STORAGE_TOPIC=$(echo $kafka_prefix)connect-offsets
-OFFSET_STORAGE_TOPIC=$(echo $kafka_prefix)connect-offsets
+export CONNECT_OFFSET_STORAGE_TOPIC=$(echo $kafka_prefix)connect-offsets
+#OFFSET_STORAGE_TOPIC=$(echo $kafka_prefix)connect-offsets
 
-#CONNECT_CONFIG_STORAGE_TOPIC=$(echo $kafka_prefix)connect-configs
-CONFIG_STORAGE_TOPIC=$(echo $kafka_prefix)connect-configs
+export CONNECT_CONFIG_STORAGE_TOPIC=$(echo $kafka_prefix)connect-configs
+#CONFIG_STORAGE_TOPIC=$(echo $kafka_prefix)connect-configs
 
-#CONNECT_STATUS_STORAGE_TOPIC=$(echo $kafka_prefix)connect-status
-STATUS_STORAGE_TOPIC=$(echo $kafka_prefix)connect-status
+export CONNECT_STATUS_STORAGE_TOPIC=$(echo $kafka_prefix)connect-status
+#STATUS_STORAGE_TOPIC=$(echo $kafka_prefix)connect-status
 
 echo "======== After CONNECT_STATUS_STORAGE_TOPIC ====="
 
@@ -65,14 +53,16 @@ echo "======== After CONNECT_STATUS_STORAGE_TOPIC ====="
 #CONNECT_PLUGIN_PATH=/usr/share/java
 
 
-KEY_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
-VALUE_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
-INTERNAL_KEY_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
-INTERNAL_VALUE_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
-REST_ADVERTISED_HOST_NAME="localhost"
-PLUGIN_PATH=/usr/share/java
+#KEY_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
+#VALUE_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
+#INTERNAL_KEY_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
+#INTERNAL_VALUE_CONVERTER="org.apache.kafka.connect.json.JsonConverter"
+#REST_ADVERTISED_HOST_NAME="localhost"
+#PLUGIN_PATH=/usr/share/java
 
 echo "======== After CONNECT_PLUGIN_PATH ====="
+
+echo " Server URL $SERVER_URL "
 
 curl -vvv -X POST -H "Content-Type: application/json" --data /etc/kafka/connect-distributed.properties $SERVER_URL/connectors
 sleep infinity
