4c982b0d9cd71e033dccbc68607c778d65665383
mmegna <75279388+mmegna@users.noreply.github.com>
2021-02-02 14:16:02 -0700

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -29,6 +29,58 @@ export CONNECT_REST_ADVERTISED_HOST_NAME="$SERVER_HOST"
 
 echo "======== After PORT ====="
 
+#set -e
+
+#addon="$1"
+
+#[ -z $addon ] && {
+  #echo "addon is missing" >&2
+  #exit 1
+#}
+
+#client_key="$(echo $addon)_CLIENT_CERT_KEY"
+#client_cert="$(echo $addon)_CLIENT_CERT"
+#trusted_cert="$(echo $addon)_TRUSTED_CERT"
+
+client_key=$KAFKA_CLIENT_CERT_KEY
+client_cert=$KAFKA_CLIENT_CERT
+trusted_cert=$KAFKA_TRUSTED_CERT
+
+[ -z $TRUSTSTORE_PASSWORD ] && {
+  echo "TRUSTSTORE_PASSWORD is missing" >&2
+  exit 1
+}
+
+[ -z $KEYSTORE_PASSWORD ] && {
+  echo "KEYSTORE_PASSWORD is missing" >&2
+  exit 1
+}
+
+rm -f .{keystore,truststore}.{pem,pkcs12,jks}
+rm -f .cacerts
+
+#echo -n "${!client_key}" >> /etc/kafka/client_key.pem
+#echo -n "${!client_cert}" >>  /etc/kafka/client_cert.pem
+#echo -n "${!trusted_cert}" >  /etc/kafka/truststore.pem
+
+echo -n "$client_key" >>   /etc/kafka/client_key.pem
+echo -n "$client_cert" >>  /etc/kafka/client_cert.pem
+echo -n "$trusted_cert" >  /etc/kafka/truststore.pem
+echo -ne "test" > /etc/kafka/test.txt
+touch /etc/kafka/test1.txt
+
+echo "keystore - $ /etc/kafka/keystore.pem"
+echo "trusted - $ /etc/kafka/truststore.pem"
+
+keytool -importcert -file  /etc/kafka/truststore.pem -keystore  /etc/kafka/truststore.jks -deststorepass $TRUSTSTORE_PASSWORD -noprompt
+
+openssl pkcs12 -export -in  /etc/kafka/client_cert.pem -inkey  /etc/kafka/client_key.pem -out  /etc/kafka/keystore.pkcs12 -password pass:$KEYSTORE_PASSWORD
+keytool -importkeystore -srcstoretype PKCS12 \
+    -destkeystore  /etc/kafka/keystore.jks -deststorepass $KEYSTORE_PASSWORD \
+    -srckeystore  /etc/kafka/keystore.pkcs12 -srcstorepass $KEYSTORE_PASSWORD
+
+#rm -f .{keystore,truststore}.{pem,pkcs12}
+
 kafka_addon_name=${KAFKA_ADDON:-KAFKA}
 prefix_env_var="$(echo $kafka_addon_name)_PREFIX"
 kafka_prefix=$(echo ${!prefix_env_var})
