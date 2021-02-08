31247c263bb650bd0d0dbc0859359946eac2b5b5
mmegna <75279388+mmegna@users.noreply.github.com>
2021-02-08 15:50:26 -0700

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -178,5 +178,6 @@ echo "Heroku Port - $CONNECT_REST_PORT"
 #wget https://repo1.maven.org/maven2/com/snowflake/snowflake-kafka-connector/1.5.1/snowflake-kafka-connector-1.5.1.jar
 #cp snowflake-kafka-connector-1.5.1.jar /etc/kafka
 
-curl -vvv -X POST -H "Content-Type: application/json" --data /etc/kafka-connect/connect-distributed.properties $SERVER_URL/connectors
+#curl -vvv -X POST -H "Content-Type: application/json" --data /etc/kafka-connect/connect-distributed.properties $SERVER_URL/connectors
+curl -vvv -X POST -H "Content-Type: application/json" --data /etc/kafka-connect/connect-distributed.properties $SERVER_URL
 sleep infinity
