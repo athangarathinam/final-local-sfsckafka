69f0d73c658af54a9e0e445da4b33ba92e1631ba
mmegna <75279388+mmegna@users.noreply.github.com>
2021-02-09 15:56:32 -0700

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -29,66 +29,6 @@ export RANDFILE=/etc/kafka-connect/.rnd
 
 #echo "======== After PORT ====="
 
-#[ -z $addon ] && {
- # echo "addon is missing" >&2
-  #exit 1
-#}
-
-#client_key="$(echo $addon)_CLIENT_CERT_KEY"
-#client_cert="$(echo $addon)_CLIENT_CERT"
-#trusted_cert="$(echo $addon)_TRUSTED_CERT"
-
-client_key=$KAFKA_CLIENT_CERT_KEY
-client_cert=$KAFKA_CLIENT_CERT
-trusted_cert=$KAFKA_TRUSTED_CERT
-
-[ -z $TRUSTSTORE_PASSWORD ] && {
-  echo "TRUSTSTORE_PASSWORD is missing" >&2
-  exit 1
-}
-
-[ -z $KEYSTORE_PASSWORD ] && {
-  echo "KEYSTORE_PASSWORD is missing" >&2
-  exit 1
-}
-
-rm -f .{keystore,truststore}.{pem,pkcs12,jks}
-rm -f .cacerts
-
-#echo -n "${!client_key}" >> /etc/kafka-connect/client_key.pem
-#echo -n "${!client_cert}" >>  /etc/kafka-connect/client_cert.pem
-#echo -n "${!trusted_cert}" >  /etc/kafka-connect/truststore.pem
-
-echo -n "$client_key" >   /etc/kafka-connect/client_key.pem
-echo -n "$client_cert" >  /etc/kafka-connect/client_cert.pem
-echo -n "$trusted_cert" >  /etc/kafka-connect/truststore.pem
-
-
-
-if [ "$?" = "0" ]; then
-  echo "No Error while creating .pem files"
-else
-  echo "Error while creating .pem files"
-  exit 1
-fi
-
-echo "keystore - $ /etc/kafka-connect/client_key.pem"
-echo "trusted - $ /etc/kafka-connecta/client_cert.pem"
-echo "trusted - $ /etc/kafka-connecta/truststore.pem"
-
-keytool -importcert -file  /etc/kafka-connect/truststore.pem -keystore  /etc/kafka-connect/truststore.jks -deststorepass $TRUSTSTORE_PASSWORD -noprompt
-
-openssl pkcs12 -export -in  /etc/kafka-connect/client_cert.pem -inkey  /etc/kafka-connect/client_key.pem -out  /etc/kafka-connect/keystore.pkcs12 -password pass:$KEYSTORE_PASSWORD
-keytool -importkeystore -srcstoretype PKCS12 \
-    -destkeystore  /etc/kafka-connect/keystore.jks -deststorepass $KEYSTORE_PASSWORD \
-    -srckeystore  /etc/kafka-connect/keystore.pkcs12 -srcstorepass $KEYSTORE_PASSWORD
-
-#rm -f .{keystore,truststore}.{pem,pkcs12}
-
-echo "Client Cert Key: CK-$client_key"
-echo "Client Cert: TP-$client_cert" 
-echo "Trusted Cert: KP-$trusted_cert"
-
 #kafka_addon_name=${KAFKA_ADDON:-KAFKA}
 #prefix_env_var="$(echo $kafka_addon_name)_PREFIX"
 #kafka_prefix=$(echo ${!prefix_env_var})
