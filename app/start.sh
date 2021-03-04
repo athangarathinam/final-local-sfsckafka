13d80b2fa9391d985629524a7f65aca6c03fef35
athangarathinam <74936344+athangarathinam@users.noreply.github.com>
2021-03-04 16:34:58 +0530

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -6,6 +6,7 @@ echo "======== $APP_NAME ====="
 SERVER_HOST="$(echo $APP_NAME).herokuapp.com"
 #SERVER_URL=https://$SERVER_HOST
 SERVER_URL=http://$SERVER_HOST
+
 export CONNECT_REST_ADVERTISED_HOST_NAME=$(echo $APP_NAME).herokuapp.com
 export CONNECT_KAFKA_HEAP_OPTS="-Xms256M -Xmx2G"
 KAFKA_HEAP_OPTS="-Xms256M -Xmx1G"
